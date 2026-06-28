26d00dd9265fd33d464ec6774cd578857932a12f
hieulm <luu7940@gmail.com>
2026-06-28 15:59:25 +0700

    retry http

--- backend/src/embedding/providers/jina.provider.ts
+++ backend/src/embedding/providers/jina.provider.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { ConfigService } from '@nestjs/config';
+import { retry } from '../../helpers/http/httpRetry';
+import { AxiosResponse } from 'axios';
 
 type EMBED_DATA = {
   object: string;
@@ -38,22 +40,24 @@ export class JinaProvider {
     input: string | string[],
     task: string,
   ): Promise<RESPONSE_JINA> {
-    const { data } = await this.httpService.axiosRef.post<RESPONSE_JINA>(
-      'https://api.jina.ai/v1/embeddings',
-      {
-        embedding_type: 'float',
-        task,
-        input,
-        model: this.MODEL_NAME,
-        normalized: true,
-        truncate: false,
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${this.API_KEY}`,
+    const { data } = await retry<AxiosResponse<RESPONSE_JINA>>(() => {
+      return this.httpService.axiosRef.post<RESPONSE_JINA>(
+        'https://api.jina.ai/v1/embeddings',
+        {
+          embedding_type: 'float',
+          task,
+          input,
+          model: this.MODEL_NAME,
+          normalized: true,
+          truncate: false,
         },
-      },
-    );
+        {
+          headers: {
+            Authorization: `Bearer ${this.API_KEY}`,
+          },
+        },
+      );
+    });
 
     return data;
   }
---
+++ backend/src/helpers/http/httpRetry.ts
@@ -0,0 +1,26 @@
+export async function retry<TReturnValue, TResponse = any>(
+  fn: () => Promise<TReturnValue>,
+  maxRetry = 5,
+): Promise<TReturnValue> {
+  let attempt = 0;
+
+  while (true) {
+    try {
+      return await fn();
+    } catch (error: any) {
+      attempt++;
+      console.log(error);
+      const status = error?.response?.status;
+
+      const shouldRetry = !status || [429, 500, 502, 503, 504].includes(status);
+
+      if (!shouldRetry || attempt > maxRetry) {
+        throw error;
+      }
+
+      const delay = 500 * 2 ** (attempt - 1);
+
+      await new Promise((resolve) => setTimeout(resolve, delay));
+    }
+  }
+}
